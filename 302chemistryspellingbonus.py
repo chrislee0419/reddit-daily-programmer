5aac1495883199107912dcef868d6fcc2dbec539
chrislee0419 <cla235@sfu.ca>
2017-02-08 23:06:45 -0800

    302: finished, uses dynamic programming

--- 302chemistryspellingbonus.py
+++ 302chemistryspellingbonus.py
@@ -1,73 +1,89 @@
-# 02/06/2017 - Challenge 302 - Spelling with Chemisty
+# 02/06/2017 - Challenge 302 - Spelling with Chemisty (with bonus)
 # https://www.reddit.com/r/dailyprogrammer/comments/5seexn/20170206_challenge_302_easy_spelling_with/
 #
 
 import sys
 
 elements = {
-	("actinium", "ac", 227), ("aluminum", "al", 26.9815), ("americium", "am", 243),
-	("antimony", "sb", 121.75), ("argon", "ar", 39.948), ("arsenic", "as", 74.9216),
-	("astatine", "at", 210), ("barium", "ba", 137), ("berkelium", "bk", 247),
-	("beryllium", "be", 9.0122), ("bismuth", "bi", 208.98), ("boron", "b", 10.81),
-	("bromine", "br", 79.904), ("cadmium", "cd", 112.4), ("calcium", "ca", 40.08),
-	("californium", "cf", 251), ("carbon", "c", 12.011), ("cerium", "ce", 140.12),
-	("cesium", "cs", 132.9054), ("chlorine", "cl", 35.453), ("chromium", "cr", 51.996),
-	("cobalt", "co", 58.9332), ("copper", "cu", 63.546), ("curium", "cm", 247),
-	("dysprosium", "dy", 162.5), ("einsteinium", "es", 254), ("erbium", "er", 167.26),
-	("europium", "eu", 151.96), ("fermium", "fm", 257), ("fluorine", "f", 18.9984),
-	("francium", "fr", 223), ("gadolinium", "gd", 157.25), ("gallium", "ga", 69.72),
-	("germanium", "ge", 72.59), ("gold", "au", 196.966), ("hafnium", "hf", 178.49),
-	("helium", "he", 4.0026), ("holmium", "ho", 164.93), ("hydrogen", "h", 1.0079),
-	("indium", "in", 114.82), ("iodine", "i", 126.904), ("iridium", "ir", 192.22),
-	("iron", "fe", 55.847), ("krypton", "kr", 83.8), ("lanthanum", "la", 138.905),
-	("lawrencium", "lr", 256), ("lead", "pb", 207.2), ("lithium", "li", 6.941),
-	("lutetium", "lu", 174.97), ("magnesium", "mg", 24.305), ("manganese", "mn", 54.938),
-	("mendelevium", "md", 258), ("mercury", "hg", 200.59), ("molybdenum", "mo", 95.94),
-	("neodymium", "nd", 144.24), ("neon", "ne", 20.179), ("neptunium", "np", 237.048),
-	("nickel", "ni", 58.7), ("niobium", "nb", 92.9064), ("nitrogen", "n", 14.0067),
-	("nobelium", "no", 255), ("osmium", "os", 190.2), ("oxygen", "o", 15.9994),
-	("palladium", "pd", 106.4), ("phosphorus", "p", 30.9738), ("platinum", "pt", 195.09),
-	("plutonium", "pu", 244), ("polonium", "po", 210), ("potassium", "k", 39.098),
-	("praseodymium", "pr", 140.908), ("promethium", "pm", 147), ("protactinium", "pa", 231.036),
-	("radium", "ra", 226.025), ("radon", "rn", 222), ("rhenium", "re", 186.207),
-	("rhodium", "rh", 102.906), ("rubidium", "rb", 85.4678), ("ruthenium", "ru", 101.07),
-	("rutherfordium", "rf", 261), ("samarium", "sm", 150.4), ("scandium", "sc", 44.9559),
-	("selenium", "se", 78.96), ("silicon", "si", 28.086), ("silver", "ag", 107.868),
-	("sodium", "na", 22.9898), ("strontium", "sr", 87.62), ("sulfur", "s", 32.06),
-	("tantalum", "ta", 180.948), ("technetium", "tc", 98.9062), ("tellurium", "te", 127.6),
-	("terbium", "tb", 158.925), ("thallium", "tl", 204.37), ("thorium", "th", 232.038),
-	("thulium", "tm", 168.934), ("tin", "sn", 118.69), ("titanium", "ti", 47.9),
-	("tungsten", "w", 183.85), ("uranium", "u", 238.029), ("vanadium", "v", 50.9414),
-	("xenon", "xe", 131.3), ("ytterbium", "yb", 173.04), ("yttrium", "y", 88.9059),
-	("zinc", "zn", 65.38), ("zirconium", "zr", 91.22)
+	"ac": ("actinium", 227), "al": ("aluminum", 26.9815), "am": ("americium", 243),
+	"sb": ("antimony", 121.75), "ar": ("argon", 39.948), "as": ("arsenic", 74.9216),
+	"at": ("astatine", 210), "ba": ("barium", 137), "bk": ("berkelium", 247),
+	"be": ("beryllium", 9.0122), "bi": ("bismuth", 208.98), "b": ("boron", 10.81),
+	"br": ("bromine", 79.904), "cd": ("cadmium", 112.4), "ca": ("calcium", 40.08),
+	"cf": ("californium", 251), "c": ("carbon", 12.011), "ce": ("cerium", 140.12),
+	"cs": ("cesium", 132.9054), "cl": ("chlorine", 35.453), "cr": ("chromium", 51.996),
+	"co": ("cobalt", 58.9332), "cu": ("copper", 63.546), "cm": ("curium", 247),
+	"dy": ("dysprosium", 162.5), "es": ("einsteinium", 254), "er": ("erbium", 167.26),
+	"eu": ("europium", 151.96), "fm": ("fermium", 257), "f": ("fluorine", 18.9984),
+	"fr": ("francium", 223), "gd": ("gadolinium", 157.25), "ga": ("gallium", 69.72),
+	"ge": ("germanium", 72.59), "au": ("gold", 196.966), "hf": ("hafnium", 178.49),
+	"he": ("helium", 4.0026), "ho": ("holmium", 164.93), "h": ("hydrogen", 1.0079),
+	"in": ("indium", 114.82), "i": ("iodine", 126.904), "ir": ("iridium", 192.22),
+	"fe": ("iron", 55.847), "kr": ("krypton", 83.8), "la": ("lanthanum", 138.905),
+	"lr": ("lawrencium", 256), "pb": ("lead", 207.2), "li": ("lithium", 6.941),
+	"lu": ("lutetium", 174.97), "mg": ("magnesium", 24.305), "mn": ("manganese", 54.938),
+	"md": ("mendelevium", 258), "hg": ("mercury", 200.59), "mo": ("molybdenum", 95.94),
+	"nd": ("neodymium", 144.24), "ne": ("neon", 20.179), "np": ("neptunium", 237.048),
+	"ni": ("nickel", 58.7), "nb": ("niobium", 92.9064), "n": ("nitrogen", 14.0067),
+	"no": ("nobelium", 255), "os": ("osmium", 190.2), "o": ("oxygen", 15.9994), "pd": ("palladium", 106.4),
+	"p": ("phosphorus", 30.9738),  "pt": ("platinum", 195.09), "pu": ("plutonium", 244),
+	"po": ("polonium", 210), "k": ("potassium", 39.098), "pr": ("praseodymium", 140.908),
+	"pm": ("promethium", 147), "pa": ("protactinium", 231.036), "ra": ("radium", 226.025),
+	"rn": ("radon", 222), "re": ("rhenium", 186.207), "rh": ("rhodium", 102.906),
+	"rb": ("rubidium", 85.4678), "ru": ("ruthenium", 101.07), "rf": ("rutherfordium", 261),
+	"sm": ("samarium", 150.4), "sc": ("scandium", 44.9559), "se": ("selenium", 78.96),
+	"si": ("silicon", 28.086), "ag": ("silver", 107.868), "na": ("sodium", 22.9898),
+	"sr": ("strontium", 87.62), "s": ("sulfur", 32.06), "ta": ("tantalum", 180.948),
+	"tc": ("technetium", 98.9062), "te": ("tellurium", 127.6), "tb": ("terbium", 158.925),
+	"tl": ("thallium", 204.37), "th": ("thorium", 232.038), "tm": ("thulium", 168.934),
+	"sn": ("tin", 118.69), "ti": ("titanium", 47.9), "w": ("tungsten", 183.85),
+	"u": ("uranium", 238.029), "v": ("vanadium", 50.9414), "xe": ("xenon", 131.3),
+	"yb": ("ytterbium", 173.04), "y": ("yttrium", 88.9059), "zn": ("zinc", 65.38),
+	"zr": ("zirconium", 91.22)
 }
 
 def build(word):
 	if (word == ''):
-		return []
+		return [[], 0]
+	res = 0
+	two = 0
+	one = 0
+	if (len(word) > 1):
+		try:
+			el = word[0:2]
+			name = (elements[el])[0]
+			weight = (elements[el])[1]
+			two = build(word[2:])
+			if (type(two) is list):
+				two[0].insert(0, (el, name))
+				two[1] += weight
+		except KeyError:
+			pass
 	try:
 		el = word[0:1]
-		name = elements[el]
-		res = build(word[1:])
-		if (type(res) is list):
-			res.insert(0, (el, name))	
-			return res
+		name = (elements[el])[0]
+		weight = (elements[el])[1]
+		one = build(word[1:])
+		if (type(one) is list):
+			one[0].insert(0, (el, name))
+			one[1] += weight
 	except KeyError:
 		pass
-	if (len(word) < 2):
-		return 0
-	try:
-		el = word[0:2]
-		name = elements[el]
-		res = build(word[2:])
-		if (type(res) is list):
-			res.insert(0, (el, name))
-			return res
-	except KeyError:
-		pass
-	return 0
+	if (type(two) is list and type(one) is list):
+		if (one[1] > two[1]):
+			res = one
+			del two
+		else:
+			res = two
+			del one
+	elif (type(two) is list):
+		res = two
+	else:
+		res = one
+	return res
 
-def print_res(tuples):
+def print_res(res):
+	tuples = res[0]
 	names = []
 	s = ""
 	for t in tuples:
@@ -78,7 +94,8 @@ def print_res(tuples):
 	for i in range(len(names)-1):
 		s += names[i] + ", "
 	s += names[len(names)-1] + ")"
-	print s
+	print s,
+	print "[" + str(res[1]) + "]"
 
 res = build(sys.argv[1].lower())
 if (type(res) is int):
