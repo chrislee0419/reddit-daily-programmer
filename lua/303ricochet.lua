0ad23d55bc34638c97d6af955450093e0340a7a1
chrislee0419 <cla235@sfu.ca>
2017-02-22 01:26:14 -0800

    303: finished easy with bonus (used lcm)

---
+++ lua/303ricochet.lua
@@ -0,0 +1,24 @@
+-- 02/21/2017 - Challenge 303 - Ricochet (with bonus)
+-- https://www.reddit.com/r/dailyprogrammer/comments/5vb1wf/20170221_challenge_303_easy_ricochet/
+-- 
+-- Usage:
+-- (height_of_box) (width_of_box) (height_of_particle) (width_of_particle) (speed)
+
+local vert, hori = 0, 0
+local dy, dx = tonumber(arg[1]) - tonumber(arg[3]), tonumber(arg[2]) - tonumber(arg[4])
+local left, up = true, true
+while true
+do
+	if vert < hori then
+		vert = vert + dy
+		up = not up
+	else
+		hori = hori + dx
+		left = not left
+	end
+	if vert == hori then
+		local c = left and "LL" or up and "UR" or "LR"
+		print(c, hori/dx + vert/dy - 2, vert / tonumber(arg[5]))
+		return
+	end
+end
